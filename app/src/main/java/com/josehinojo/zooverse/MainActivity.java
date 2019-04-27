fbe07bef1162f67bc304accb6f35fae7ef2934ab
Jose Hinojo <burningremedy@gmail.com>
2019-04-27 14:49:57 -0500

    Add BottomNav

--- app/src/main/java/com/josehinojo/zooverse/MainActivity.java
+++ app/src/main/java/com/josehinojo/zooverse/MainActivity.java
@@ -1,9 +1,22 @@
 package com.josehinojo.zooverse;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomAppBar bottomAppBar;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        bottomAppBar = (BottomAppBar) findViewById(R.id.bottomApp);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.cameraFab);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "stop", Toast.LENGTH_SHORT).show();
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        setSupportActionBar(bottomAppBar);
+
+
         //shtopiahd;oaisjdf;ojsdf
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottommenu,menu);
+        return true;
     }
 }
