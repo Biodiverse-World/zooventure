868f4ec8ff8a76ac2f98e916df459737c0a4b24f
Jose Hinojo <burningremedy@gmail.com>
2019-04-27 21:26:05 -0500

    Add Animal POJO

--- app/src/main/java/com/josehinojo/zooverse/MainActivity.java
+++ app/src/main/java/com/josehinojo/zooverse/MainActivity.java
@@ -11,6 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.josehinojo.zooverse.POJOS.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
     BottomAppBar bottomAppBar;
     FloatingActionButton floatingActionButton;
 
+    FirebaseDatabase database;
+    DatabaseReference zooReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        zooReference = database.getReference("animals");
+
+        Animal animal = new Animal("white stork","habitat alteration, shortage of nesting sites, excessive use of pesticides, collision with and electrocution from overhead powerlines, hunted for food and sport","Bird",
+                "https://www.iucnredlist.org/species/22697691/86248677","Ciconia ciconia"
+        ,"Grassland, Artificial/Terrestrial","Europe, North Africa, Asia Minor, and the Middle East",
+                "Least Concern","White Stork","BirdLife International 2016. Ciconia ciconia. The IUCN Red List of Threatened Species 2016: e.T22697691A86248677. http://dx.doi.org/10.2305/IUCN.UK.2016-3.RLTS.T22697691A86248677.en. Downloaded on 27 April 2019.");
+
+
 
 
         bottomAppBar = (BottomAppBar) findViewById(R.id.bottomApp);
---
+++ app/src/main/java/com/josehinojo/zooverse/POJOS/Animal.java
@@ -0,0 +1,112 @@
+package com.josehinojo.zooverse.POJOS;
+
+public class Animal {
+
+    private String name;
+    private String threats;
+    private String taxon;
+    private String source;
+    private String scientificName;
+    private String habitat;
+    private String geoRange;
+    private String conservationStatus;
+    private String commonName;
+    private String Citation;
+
+    public Animal(String name, String threats, String taxon, String source, String scientificName, String habitat, String geoRange, String conservationStatus, String commonName, String citation) {
+        this.name = name;
+        this.threats = threats;
+        this.taxon = taxon;
+        this.source = source;
+        this.scientificName = scientificName;
+        this.habitat = habitat;
+        this.geoRange = geoRange;
+        this.conservationStatus = conservationStatus;
+        this.commonName = commonName;
+        Citation = citation;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThreats() {
+        return threats;
+    }
+
+    public void setThreats(String threats) {
+        this.threats = threats;
+    }
+
+    public String getTaxon() {
+        return taxon;
+    }
+
+    public void setTaxon(String taxon) {
+        this.taxon = taxon;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getGeoRange() {
+        return geoRange;
+    }
+
+    public void setGeoRange(String geoRange) {
+        this.geoRange = geoRange;
+    }
+
+    public String getConservationStatus() {
+        return conservationStatus;
+    }
+
+    public void setConservationStatus(String conservationStatus) {
+        this.conservationStatus = conservationStatus;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getCitation() {
+        return Citation;
+    }
+
+    public void setCitation(String citation) {
+        Citation = citation;
+    }
+}
