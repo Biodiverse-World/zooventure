84ff11f17184af2dadf993b44fd2bd0def38ad2f
Chris Rodiguez <christophererodriguez70@gmail.com>
2019-06-01 01:48:41 -0500

    UPDATE: Added orange color Updated list layout with cardviews Adjusted padding, text, alignment and margin for recyclerview, cards, and textfields

--- app/src/main/java/com/josehinojo/zooverse/SplashActivity.java
+++ app/src/main/java/com/josehinojo/zooverse/SplashActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
